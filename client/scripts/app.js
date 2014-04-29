1a2ff487f64156c20bd9209acd16fc34010b7be8
Hack Reactor Students <keiwi.g@gmail.com>
2014-04-28 17:33:02 -0700

    added user input form

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,29 +9,79 @@ var app = {
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message sent');
+        //console.log(data);
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message');
+        //console.log(data);
+      }
+    });
+  },
+  fetch : function(){
+    $.ajax(app.server, {
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        var results = data.results;
+        //console.log(data);
+        for(var i = 0; i < results.length; i++){
+          var message = "<div class='message'></div>";
+
+          $('#main').append(message);
+          $('.message:last').text(results[i]['username']+": "+ results[i]["text"]);
+        }
       },
       error: function (data) {
         console.error('chatterbox: Failed to send message');
       }
     });
   },
-  fetch : function(){
-    $.ajax({
-      url: 'https://api.parse.com/1/classes/chatterbox',
-      type: 'GET',
-      //data: JSON.stringify(),
-      contentType: 'application/json',
-      success: function (data) {
-        return data;
-      },
-      error: function (data) {
 
-        console.error('chatterbox: Failed to send message');
-      }
-    });
-  }
+  server : 'https://api.parse.com/1/classes/chatterbox'
+
+
 };
+$(document).ready(function (){
+
+  app.fetch(); // On page load, retrieve messages
+  $(".refresh").on("click", function(){
+    $(".message").remove();
+    app.fetch();
+  });
+
+});
 
 
-// $.ajax( { } );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- spec/chatterboxSpec.js
+++ spec/chatterboxSpec.js
@@ -56,6 +56,7 @@ describe('chatterbox', function() {
       it('should submit a GET request via $.ajax', function(done){
         app.fetch();
         expect($.ajax.calledOnce).to.be.true;
+        console.log($.ajax.args);
         expect($.ajax.args[0][0]).to.equal(app.server);
         done();
       });
